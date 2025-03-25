e438cfe52bb8c71bf72751d3c74f14fe0f14018a
Miguel2527 <miguelavellaneda2527@gmail.com>
2025-03-25 16:40:10 -0500

    345

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 function Loguear(){
     let username = document.getElementById('username').value
-    let password = document.getElementById('clave').value
+    let password = document.getElementById('password').value
 
     if(username=='rodolfo' && password=='123456'){
         alert('usuario valido')
--- js/registro.js
+++ js/registro.js
@@ -3,9 +3,20 @@ function regis(){
     let edad=document.getElementById("age").value
     let n_identificacion=document.getElementById("iden").value
     let correo=document.getElementById("email").value
- 
-    let registro = [nombre,edad, n_identificacion, correo]
+    let usuario=document.getElementById("usuario").value
+    let clave=document.getElementById("clave").value
+    let clave2=document.getElementById("clave2").value
 
-    console.log(registro)
+    if(clave == clave2){
+
+        alert("tas bien")
+        let registro = [nombre, edad, n_identificacion, correo, usuario, clave]
+
+        console.log(registro) 
+    }else{
+        alert("tas mal")
+    }
+
+
 
 }
