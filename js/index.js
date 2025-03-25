c8cd627bdea0d137fb84022c9d02f034a75cee6e
Miguel2527 <miguelavellaneda2527@gmail.com>
2025-03-25 15:45:30 -0500

    6 cambio

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+function Loguear(){
+    let username = document.getElementById('username').value
+    let password = document.getElementById('clave').value
+
+    if(username=='rodolfo' && password=='123456'){
+        alert('usuario valido')
+    }else{
+        alert('usuario invalido')
+    }
+}
